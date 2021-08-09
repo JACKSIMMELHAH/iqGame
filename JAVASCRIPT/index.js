a663c86f99ddca103cf7ae8c23bbbaddd617ea25
Jack <simmelhag64@gmail.com>
2021-08-09 20:51:48 +0200

    graphic improvement

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,11 +1,11 @@
-var sex = 0;
+var sex = 0.00;
 var money = 0;
 var sexMulti = 1;
 var costMulti = 0.05;
 
 
 function addSex() {
-    var showSex = `You have this many Sex:  ${(sex += 0.1 * sexMulti).toFixed(2)}`;
+    var showSex = `${(sex += 0.1 * sexMulti).toFixed(2)}`;
     document.getElementById("showSex").innerHTML = showSex
 }
 
@@ -14,7 +14,7 @@ function sellSex() {
         var totalMoney = `Money:  ${(money += 1 * 0.03).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
 
-        var showSex = `You have this many Sex:  ${(sex -= 0.999999999).toFixed(2)}`;
+        var showSex = `${(sex -= 0.999999999).toFixed(2)}`;
         document.getElementById("showSex").innerHTML = showSex
     } else return
 }
@@ -25,11 +25,11 @@ function addSexMulti() {
         var totalMoney = `Money:  ${(money -= costMulti).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
 
-        var showMulti = `Sex Multi:  ${(sexMulti += 0.1).toFixed(1)}x`;
+        var showMulti = `${(sexMulti += 0.1).toFixed(1)}x`;
         document.getElementById("showMulti").innerHTML = showMulti
 
         var showCostMulti = costMulti += 0.025
-        return showCostMulti
+        document.getElementById("showCostMulti").innerHTML = showCostMulti
     } else return
 }
 
