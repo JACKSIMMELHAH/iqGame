e681c776506d01e8ce5fb2ec8233ab66392bbf2c
Jack <simmelhag64@gmail.com>
2021-08-09 15:32:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var sex = 0;
 var money = 0;
 var sexMulti = 1;
-Number.prototype.countDecimals = function () {
-
-    if (Math.floor(this.valueOf()) === this.valueOf()) return 0;
-
-    var str = this.toString();
-    if (str.indexOf(".") !== -1 && str.indexOf("-") !== -1) {
-        return str.split("-")[1] || 0;
-    } else if (str.indexOf(".") !== -1) {
-        return str.split(".")[1].length || 0;
-    }
-    return str.split("-")[1] || 0;
-}
+var costMulti = 0.025;
 
 
 function addSex() {
@@ -21,27 +10,26 @@ function addSex() {
 }
 
 function sellSex() {
-    if (sex >= (1).toFixed(0)) {
-        if (sex.countDecimals == 3) {
-            var totalMoney = `Money:  ${(money += sex * 0.03).toFixed(3)}`;
-            document.getElementById("totalMoney").innerHTML = totalMoney
-        } else var totalMoney = `Money:  ${(money += sex * 0.03).toFixed(2)}`;
+    if (sex >= 0.999999999) {
+        var totalMoney = `Money:  ${(money += 1 * 0.03).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
 
-
-        var showSex = `You have this many Sex:  ${(sex -= (1).toFixed(0)).toFixed(2)}`;
+        var showSex = `You have this many Sex:  ${(sex -= 0.999999999).toFixed(2)}`;
         document.getElementById("showSex").innerHTML = showSex
     } else return
 }
 
 function addSexMulti() {
-    if (money >= 0.05) {
-        var totalMoney = `Money:  ${(money -= 0.05).toFixed(2)}`;
-        document.getElementById("totalMoney").innerHTML = totalMoney
+    if (money >= costMulti) {
 
+        var totalMoney = `Money:  ${(money -= costMulti).toFixed(2)}`;
+        document.getElementById("totalMoney").innerHTML = totalMoney
 
         var showMulti = `Sex Multi:  ${(sexMulti += 0.1).toFixed(1)}x`;
         document.getElementById("showMulti").innerHTML = showMulti
+
+        var showCostMulti = costMulti += 0.1
+        return showCostMulti
     } else return
 }
 
