5867e85fa4b4662f9dbe4119a16af927784de28e
Jack <simmelhag64@gmail.com>
2021-08-09 21:38:11 +0200

    Changed some things

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,32 +1,32 @@
-var sex = 0.00;
+var iq = 0.00;
 var money = 0;
-var sexMulti = 1;
+var iqMulti = 1;
 var costMulti = 0.05;
 
 
-function addSex() {
-    var showSex = `${(sex += 0.1 * sexMulti).toFixed(2)}`;
-    document.getElementById("showSex").innerHTML = showSex
+function addIq() {
+    var showIq = `${(iq += 0.1 * iqMulti).toFixed(2)}`;
+    document.getElementById("showIq").innerHTML = showIq
 }
 
-function sellSex() {
-    if (sex >= 0.999999999) {
+function sellIq() {
+    if (iq >= 0.999999999) {
         var totalMoney = `Money:  ${(money += 1 * 0.03).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
 
-        var showSex = `${(sex -= 0.999999999).toFixed(2)}`;
-        document.getElementById("showSex").innerHTML = showSex
+        var showIq = `${(iq -= 0.999999999).toFixed(2)}`;
+        document.getElementById("showIq").innerHTML = showIq
     } else return
 }
 
-function addSexMulti() {
+function addIqMulti() {
     if (money >= costMulti) {
 
         var totalMoney = `Money:  ${(money -= costMulti).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
 
-        var showMulti = `${(sexMulti += 0.1).toFixed(1)}x`;
-        document.getElementById("showMulti").innerHTML = showMulti
+        var showIqMulti = `${(iqMulti += 0.1).toFixed(1)}x`;
+        document.getElementById("showIqMulti").innerHTML = showIqMulti
 
         var showCostMulti = costMulti += 0.025
         document.getElementById("showCostMulti").innerHTML = showCostMulti
