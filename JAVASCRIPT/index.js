24b68684d519e7a8e0489c2edac3e7c286ffa81a
Jack <simmelhag64@gmail.com>
2021-08-15 16:04:07 +0200

    save

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -5,7 +5,6 @@ var iqMulti = 1;
 var costMulti = 0.05;
 var minIq = 0.999999999;
 
-
 //---------------------------------------
 
 function showMenu() {
@@ -16,13 +15,13 @@ function showMenu() {
 //---------------------------------------
 
 function jobs() {
-    document.getElementById("wormsJob").style = "display: block"
+    document.getElementById("btn1").style = "display: block"
 }
 
 //---------------------------------------
 
 function learn() {
-    document.getElementById("wormsJob").style = "display: none"
+    document.getElementById("btn1").style = "display: none"
 }
 
 //---------------------------------------
@@ -58,13 +57,30 @@ function addIqMulti() {
 function eatWorms() {
     var moneyPerSeconds = `(${(moneyPerSecond += 0.1).toFixed(2)}/s)`;
     var totalMoney = `${(money += moneyPerSecond).toFixed(2)}`;
+    var wormsJob = document.getElementById("btn1");
     document.getElementById("moneyPerSecond").innerHTML = moneyPerSeconds
     document.getElementById("totalMoney").innerHTML = totalMoney
-    document.getElementById("wormsJob").disabled = true;
+    var wormsJob = document.getElementById("btn1").disabled = true;
+    if (wormsJob.disabled = true) {
+        var owning = `(owned)`
+        document.getElementById("owning").innerHTML = owning
+    }
     setInterval(function eatWorms() {
         var totalMoney = `${(money += moneyPerSecond).toFixed(2)}`;
         document.getElementById("totalMoney").innerHTML = totalMoney
     }, 1000)
 };
+
+function save(){
+var testObject = { 'money': money.toFixed(2), 'moneyPerSecond': moneyPerSecond.toFixed(2) };
+
+localStorage.setItem('testObject', JSON.stringify(testObject));
+
+var retrievedObject = localStorage.getItem('testObject');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
+}
+
+
 
 //---------------------------------------
